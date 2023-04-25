0fea6aa84550f95dc3994e77d460a0f4864eb53d
JesSonne <86361214+JesSonne@users.noreply.github.com>
2023-04-25 13:45:23 +0200

    Update sampling script.R

--- sampling script.R
+++ sampling script.R
@@ -1,15 +1,16 @@
 # visualyzing sampling algorithm
 require(terra) # for handling the geographical data and computations
 
-#loading sampling functions
-load("R functions.R")
+setwd("...Downloads/Local-richness-and-turnover-main")
 
-setwd("/Users/jespersonne/Documents/GitHub/Local-richness-and-turnover")
+#loading sampling functions
+source("R functions.R")
+
+#loading reference raster
 reference_raster=rast("Data files/reference_raster.tif")
 
 #global mountain regions downloaded from Rahbek et al. 2019: https://macroecology.ku.dk/resources/mountain_regions
-setwd("/Users/jespersonne/Downloads")
-mount=vect("mountainregions2/CMEC_Mountains_Enh2018.shp")
+mount=vect("...mountainregions2/CMEC_Mountains_Enh2018.shp")
 mount=project(mount,reference_raster)
 
 i=86 # example with the Andes
@@ -26,7 +27,7 @@ zero=reference_raster
 zero[]=NA
 zero[sampling_field]=1
 zero=trim(zero)
-plot(mount[po1,],col="lightgray",border="lightgray",axes=F)
+plot(mount[i,],col="lightgray",border="lightgray",axes=F)
 plot(zero,col="steelblue",add=T,legend=F,axes=F)
 
 ################################################
