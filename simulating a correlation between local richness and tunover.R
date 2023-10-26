1a00a9f91ef57c7c0d5b2febcbb57d67c2170d65
JesSonne <86361214+JesSonne@users.noreply.github.com>
2023-10-26 11:13:07 +0200

    Create simulating a correlation between local richness and tunover.R

---
+++ simulating a correlation between local richness and tunover.R
@@ -0,0 +1,67 @@
+# Simmulations showing how a positive correlation beteween local species richness and turnover can emerge
+#if there are big differences in regional species pool size among the regions to be compared (Tuomisto and Ruokolainen 2012)
+
+require(terra)
+require(parallel)
+
+setwd("C:/Users/hzc973/iCloudDrive/Documents/GitHub/Local-richness-and-turnover")
+source("R functions.R")
+
+
+#prepare the bacground raster 
+rr=rast(matrix(0,nrow=52,ncol=52))
+ras_id=rr
+ras_id[]=1:length(ras_id[])
+coords=crds(ras_id)
+sub_ras=ras_id
+sub_ras[1,]=NA
+sub_ras[,1]=NA
+sub_ras[,52]=NA
+sub_ras[52,]=NA
+
+
+set.seed(123)
+n_rep=10 # state the number of simmulations
+
+#specify the frequency distribution of total species richness (i.e., following a log-normal distribution)
+rich=round(rlnorm(n = max(rich),meanlog = 4,sdlog = 0.4))
+hist(rich)
+
+#specify the frequency distribution of species' range sizes (i.e., following a log-normal distribution)
+rs=round(rlnorm(n = max(rich),meanlog = 5,sdlog = 0.5))
+hist(rs)
+
+#running simmulations using paralell computation
+dat=data.frame(gamma=rep(NA,n_rep),alpha=NA,beta=NA)
+for(i in 1:n_rep){
+  #sample species richness and and their range sizes
+  sp_rich=rich[i]
+  ra=sample(rs,sp_rich,prob = rs,replace = F)
+  
+  #simmulate distributions (optional: specify the number of cores allocated)
+  res=mclapply(X=ra,FUN = spred_dye,dis_mat = NULL,sf1 = sub_ras)
+  
+    
+  #calculate local species richness and species turnover
+  res=unlist(res)    
+  sub_vol=ras_id
+  sub_vol[]=0
+  id=as.numeric(names(table(res)))
+  alpha=as.numeric(table(res))
+  sub_vol[id]=alpha
+  sub_vol[which(is.na(ras_id[]))]=NA
+  #plot(sub_vol,axes=F)
+  
+  dat$gamma[i]=sp_rich
+  dat$alpha[i]=mean(sub_vol[])
+  dat$beta[i]=sp_rich/mean(sub_vol[])
+  
+  
+  plot(dat$alpha,dat$beta-1,xlab="local species richness",ylab="Species turnover")
+  print(i)
+}
+
+
+
+
+
